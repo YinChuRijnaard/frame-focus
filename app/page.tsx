12fac79111338d6450f5fbb9bf4608297d51af25
Yin Chu Rijnaard <ycrijnaard@gmail.com>
2023-07-20 11:57:50 +0200

    Start of filter visuals...

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
 import FeaturedCard from "@/components/FeaturedCard";
+import Filters from "@/components/Filters";
 import AllPhotos from "@/components/AllPhotos";
 
 const Home = () => {
   return (
     <main className="page_styling">
+      {/* Feature Card */}
       <FeaturedCard />
 
-      {/* All photos */}
-      <div className="my-16 space-y-2 text-center">
+      {/* All Photos title + Filters */}
+      <div className="my-16 space-y-4 text-center">
         <h1 className="title_text font-bebas">All photos</h1>
-        <p className="desc text-sm">
+        <p className="desc text-sm sm:text-base">
           <i className="ri-information-line ri-lg"></i>&nbsp;Click on a photo to
           learn more about it
         </p>
+        <Filters />
       </div>
+
+      {/* All Photos */}
       <AllPhotos />
     </main>
   );
---
+++ components/Filters.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useState } from "react";
+
+const Filters = () => {
+  const [filtersOpen, setFiltersOpen] = useState(false);
+
+  const handleFiltersOpen = () => setFiltersOpen((prev) => !prev);
+
+  return (
+    <div className="space-y-2 text-sm sm:text-base">
+      {/* Filter button */}
+      <div
+        className="flex items-center justify-center space-x-2 cursor-pointer select-none"
+        onClick={handleFiltersOpen}
+      >
+        <i className="ri-filter-line cursor-pointer"></i>
+        <p className="underline">Filter</p>
+      </div>
+
+      {/* Filters */}
+      {filtersOpen ? (
+        <div className="grid grid-cols-3 gap-4">
+          {/* Tag(s) filter */}
+          <div className="bg-neutral-100 max-w-full px-4 py-2">
+            <p className="font-bold">Tags</p>
+            <ul className="text-left">
+              <li>Tag 1</li>
+              <li>Tag 2</li>
+              <li>Tag 3</li>
+              <li>Tag 4</li>
+              <li>Tag 5</li>
+            </ul>
+          </div>
+
+          {/* Date filter */}
+          <div className="bg-neutral-100 max-w-full px-4 py-2">
+            <p className="font-bold">Date</p>
+            <ul className="text-left">
+              <li>Newest first</li>
+              <li>Oldest first</li>
+            </ul>
+          </div>
+
+          {/* Likes filter */}
+          <div className="bg-neutral-100 max-w-full px-4 py-2">
+            <p className="font-bold">Likes</p>
+            <ul className="text-left">
+              <li>Most liked first</li>
+              <li>Least liked first</li>
+            </ul>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Filters;
